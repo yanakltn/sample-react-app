acb9840464c76fad8bd8f15cc030026c71425cc5
Yana Kalytyn <masandralive2@gmail.com>
2022-02-11 00:39:27 +0200

    fixed issue with localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
-import { configureStore, createStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from './usersSlice'
 
 const persistedState = localStorage.getItem('reduxState')
     ? JSON.parse(localStorage.getItem('reduxState'))
     : {};
 
-// const store = createStore(usersReducer, persistedState);
+
 const store = configureStore({
     reducer: {
         users: usersReducer
